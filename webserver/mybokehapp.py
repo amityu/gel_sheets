996b8b75c1fdcf3392d6636a54fc0217c83629b7
amity <amit.yuval00@gmail.com>
2023-10-12 11:48:03 +0300

    two sliders for server bokeh

--- webserver/mybokehapp.py
+++ webserver/mybokehapp.py
@@ -1,7 +1,7 @@
 import numpy as np
 from bokeh.io import push_notebook, show, output_notebook
 from bokeh.plotting import figure, row
-from bokeh.models import LinearColorMapper, Slider, ColorBar, HoverTool
+from bokeh.models import LinearColorMapper, Slider, ColorBar, HoverTool, Label, LogColorMapper
 from bokeh.layouts import column
 from bokeh.palettes import Viridis256, all_palettes, linear_palette
 from bokeh.models.sources import ColumnDataSource
@@ -28,29 +28,45 @@ MOVIE_PATH = DATA_PATH +  movie + '/'
 GRAPH_PATH = 'C:/Users/amityu/Gel_Sheet_Graph/'
 
 
+class CurveImage:
+    def __init__(self, data):
+        self.data = data
+        self.image_index = 0
+        self.num_images = data.shape[0]
+        self.source = ColumnDataSource(data={'image': [data[0,:, :]]})
+        self.p = figure(width=800, height=800, x_range=(0, 10), y_range=(0, 10), title="Curvature")
+        label = Label(x=70, y=150, x_units='screen', y_units='screen',
+                  text=r'$$K(x, y) = \frac{z_{xx} \cdot z_{yy} - (z_{xy})^2}{(1 + z_x^2 + z_y^2)^2}$$',
+                  text_font_size='16pt', background_fill_alpha=0.3, text_color = 'red')
+        self.p.add_layout(label)
+        # Create a diverging palette
+        #mapper = LogColorMapper(palette='Viridis256', low=vmin, high=vmax)
+        mapper = LinearColorMapper(palette='Viridis256', low=vmin, high=vmax)
+
+        color_bar = ColorBar(color_mapper=mapper, location=(0,0))
+
+        renderer = self.p.image(image='image', x=0, y=0, dw=10, dh=10, source=self.source, color_mapper=mapper)
+        self.p.add_layout(color_bar, 'right')
+        hover = HoverTool(renderers=[renderer], tooltips=[("x", "$x"), ("y", "$y"), ("value", "@image{0.000}")])
+        self.p.add_tools(hover)
+
+    def update_curve(self,attr, old, new):
+        if 0 <= new < self.num_images:
+            self.source.data = {'image': [self.data[new,:, :]]}
+        else:
+            raise IndexError("Image index out of bounds!")
+
+
 def modify_doc(doc):
     global spike, curvature
     print(np.nanmin(curvature), np.nanmax(curvature))
     # Example 3D data
-    data_3d = curvature  # Replace this with your 3D array
-
+    cimage = CurveImage(curvature)
     # Create ColumnDataSource
-    source1 = ColumnDataSource(data={'image': [data_3d[0,:, :]]})
+
     source2 = ColumnDataSource(data={'image': [spike[0,:, :]]})
 
     # Set up plot
-    p1 = figure(width=800, height=800, x_range=(0, 10), y_range=(0, 10), title="Curvature")
-
-    # Create a diverging palette
-
-    mapper1 = LinearColorMapper(palette='Viridis256', low=vmin, high=vmax)
-
-    color_bar1 = ColorBar(color_mapper=mapper1, location=(0,0))
-
-    renderer1 = p1.image(image='image', x=0, y=0, dw=10, dh=10, source=source1, color_mapper=LinearColorMapper(palette=Viridis256))
-    p1.add_layout(color_bar1, 'right')
-    hover1 = HoverTool(renderers=[renderer1], tooltips=[("x", "$x"), ("y", "$y"), ("value", "@image{0.000}")])
-    p1.add_tools(hover1)
 
     p2 = figure(width=800, height=800, x_range=(0, 10), y_range=(0, 10), title="surface")
     mapper2 = LinearColorMapper(palette="Viridis256", low=0, high=100)
@@ -64,25 +80,30 @@ def modify_doc(doc):
     p2.add_tools(hover2)
 
     # Set up slider
-    slider = Slider(start=0, end=data_3d.shape[0]-1, value=0, step=1, title="Index")
+    slider1 = Slider(start=0, end=spike.shape[0]-1, value=0, step=1, title="Index")
+    slider2 = Slider(start=0, end=spike.shape[0]-1, value=0, step=1, title="Index")
 
-    # Update function
     def update(attr, old, new):
-        curv_image = data_3d[slider.value,:, :].copy()
-        #curv_image[curv_image > 0] = 1
 
-        source1.data = {'image': [curv_image]}
-        source2.data = {'image': [spike[slider.value,:, :]]}
+        source2.data = {'image': [spike[slider2.value,:, :]]}
 
-
-    slider.on_change('value', update)
+    slider1.on_change('value', cimage.update_curve)
+    slider2.on_change('value', update)
 
     # Organize layout
-    layout = column(row(p1,p2), slider)
+    layout = row(column(cimage.p, slider1), column([p2,slider2], sizing_mode = 'stretch_width'), sizing_mode='stretch_width')
 
     # Add to doc
 
     doc.add_root(layout)
+    doc.template = f"""
+    {{{{ bokeh_css }}}}
+    {{{{ bokeh_js }}}}
+    <META http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    {MATHJAX_SCRIPT}
+    {{{{ plot_div|indent(8) }}}}
+    {{{{ plot_script|indent(8) }}}}
+    """
 def gaussian_curvature(surface):
     nan_mask = np.isnan(surface)
     dilated_nan_mask = binary_dilation(nan_mask)
@@ -97,8 +118,10 @@ def gaussian_curvature(surface):
     curvature[dilated_nan_mask] = np.nan
     return curvature
 
+def signed_log(x):
+    return np.sign(x[~np.isnan(x)]) * np.log1p(np.abs(x[~np.isnan(x)]))
 if __name__ == '__main__':
-    global spike, curvature
+    global spike,  vmin,vmax
     from bokeh.server.server import Server
     from bokeh.application import Application
     from bokeh.application.handlers.function import FunctionHandler
@@ -109,11 +132,16 @@ if __name__ == '__main__':
         z = img  # Some example data with noise
         curvature[t] = gaussian_curvature(z)
     curvature = curvature * 1000
-    vmin = -50
-    vmax = 50
-    print(np.nanmin(curvature), np.nanmax(curvature))
+    vmin = np.nanmin(signed_log(curvature))
+    vmax = np.nanmax(signed_log(curvature))
+    print(vmin,vmax)
+    MATHJAX_SCRIPT = """
+    <script type="text/javascript" async
+    src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.7/MathJax.js?config=TeX-MML-AM_CHTML">
+    </script>
+    """
 
     apps = {'/': Application(FunctionHandler(modify_doc))}
-    server = Server(apps, port=5004)
+    server = Server(apps, port=5005)
     server.start()
     server.io_loop.start()
