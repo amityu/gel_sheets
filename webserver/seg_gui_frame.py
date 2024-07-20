519f0de981de91cf3f699c03355b75eb9e7357d3
amityu <amit.yuval00@gmail.com>
2024-07-20 20:29:21 +0300

    Update notebook file and add corrections in preprocessing
    The notebook file '240624capture10_pp_v2.ipynb' has been renamed to '160624capture10_pp_v2.ipynb'. In the updated file, several changes have been made, added cell to rotate the images to fit horizontally, but was not set as function. minor changes made to seg_gui_frame.py

--- webserver/seg_gui_frame.py
+++ webserver/seg_gui_frame.py
@@ -3,7 +3,7 @@ import numpy as np
 from flask import Flask, render_template, jsonify
 from matplotlib.cm import ScalarMappable
 from scipy.ndimage import gaussian_filter
-
+from preprocessing import preprocessing_v2 as pp
 # app.py
 global y,t
 DATA_PATH = 'C:/Users/amityu/Gel_Sheet_Data/'
@@ -20,11 +20,13 @@ DATA_PATH = 'C:/Users/amityu/Gel_Sheet_Data/'
 #movie = 'cca120'
 #movie = 'cca40'
 #movie = 'cca300'
-movie = 'cca60_dist'
+#movie = 'cca60_dist'
+movie = '160624c10'
 #movie = 'cca120_am200'
 #movie ='280523 AM100 568_3'
 MOVIE_PATH = DATA_PATH +  movie + '/'
 TMP_PATH = 'C:/Users/amityu/Gel_Sheet_Graph/tmp/'
+ex_data = pp.get_ex_data(MOVIE_PATH)
 global y,t, image_url
 
 image_url = 'static/images/segimage.png'
@@ -32,8 +34,8 @@ plt.plot([1,2,3,4])
 plt.savefig(image_url)
 plt.close()
 
-gel = np.load(MOVIE_PATH + 'np/gel_norm.npy', mmap_mode='r')#[:, 20:,80:]
-surface = np.load(MOVIE_PATH + 'np/height55.npy', mmap_mode='r')#[:, 20:,80:]
+gel = np.load(MOVIE_PATH + 'np/gel_norm_f.npy', mmap_mode='r')#[:, 20:,80:]
+surface = np.load(MOVIE_PATH + 'np/height45_s3.npy', mmap_mode='r')#[:, 20:,80:]
 #surface = np.load(DATA_PATH + 'boundary/{}_height_std45.0.npy'.format(movie), mmap_mode='r')
 # Replace this with your actual "surface" array
 plot_sigma = 0
@@ -88,19 +90,21 @@ def update_plot():
     cbar = plt.colorbar(sm ,ax= ax2)
 
     img = gel[t,:,y,:]
-    ax1.imshow(img,origin='lower', cmap='coolwarm', aspect='auto')
+    im1 = ax1.imshow(img,origin='lower', cmap='coolwarm', aspect='auto')
     ax1.set_xlabel('X (Pixels)')
     ax1.set_ylabel('Z (Pixels)')
     ax1.set_title('Gel Corrected by illumination filter \n y=%d'%y)
 
     ax1.plot(gaussian_filter(h[y,: ],sigma = plot_sigma ), 'y', linestyle='--')
+    fig.colorbar(im1, ax =ax1)
+
     im= ax2.imshow(h, origin='lower', cmap='coolwarm', vmax = vmax, vmin = vmin)
     ax2.hlines(y=y, xmin=0, xmax=h.shape[0], color='b')
 
     ax2.set_xlabel('X (Pixels)')
     ax2.set_ylabel('Y (Pixels)')
     ax2.set_title('Surface computed \n y=%d'%y)
-    fig.suptitle('Segmentation Time=%d \n Gel %s' % (t,movie))
+    fig.suptitle('Segmentation Time=%d \n Gel %s' % (t,ex_data['name']))
 
     plt.savefig(image_url)
     plt.close()
@@ -110,7 +114,7 @@ def update_plot():
 
 if __name__ == '__main__':
 
-    app.run(debug=True, host= '0.0.0.0' , port=5000)
+    app.run(debug=True, host= '0.0.0.0' , port=5045)
     print('hello')
 
 
